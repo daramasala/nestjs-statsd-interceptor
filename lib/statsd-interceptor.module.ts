be01aa95d08641691f6b8a282aed0e8cd8f8604f
Doron Tohar <doron@sportstraders.io>
2019-11-03 09:30:05 +0200

    rename module file

---
+++ lib/statsd-interceptor.module.ts
@@ -0,0 +1,33 @@
+import { Module, Global, DynamicModule } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import {
+  StatsDInterceptor,
+  StatsDInterceptorOptions,
+  STATSD_INTERCEPTOR_OPTIONS_PROVIDER,
+} from './statsd.interceptor';
+
+const provider = {
+  provide: APP_INTERCEPTOR,
+  useClass: StatsDInterceptor,
+};
+
+@Global()
+@Module({})
+export class StatsDInterceptorModule {
+  static configure(options: StatsDInterceptorOptions = {}): DynamicModule {
+    return {
+      module: StatsDInterceptorModule,
+      providers: [
+        {
+          provide: STATSD_INTERCEPTOR_OPTIONS_PROVIDER,
+          useValue: options,
+        },
+        {
+          provide: APP_INTERCEPTOR,
+          useClass: StatsDInterceptor,
+        },
+      ],
+      // exports: [provider],
+    };
+  }
+}
